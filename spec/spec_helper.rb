00cb1b82d895f946d55d63f0fe388ae460e3675b
tpoperecinii <monomachus85@yahoo.com>
2010-12-15 16:57:44 +0200

    Finished with layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,11 @@ Spork.prefork do
     require File.dirname(__FILE__) + "/../config/environment"
   end
   require 'rspec/rails'
-
+  
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
+  
   Rspec.configure do |config|
     # == Mock Framework
     #
@@ -24,14 +24,14 @@ Spork.prefork do
     # config.mock_with :flexmock
     # config.mock_with :rr
     config.mock_with :rspec
-
+    
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+    
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
-
+    
     ### Part of a Spork hack. See http://bit.ly/arY19y
     # Emulate initializer set_clear_dependencies_hook in
     # railties/lib/rails/application/bootstrap.rb
