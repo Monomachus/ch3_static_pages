bb368521c7a0285abeb12dc8fa67f2da17138821
tpoperecinii <monomachus85@yahoo.com>
2010-12-20 15:38:48 +0200

    Added some tests and improved flash rendering

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,13 @@
 module UsersHelper
-
-  def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.name,
+    def gravatar_for(user, options = { :size => 50 })
+        gravatar_image_tag(user.email.downcase, :alt => user.name,
                                             :class => 'gravatar',
                                             :gravatar => options)
-  end
+    end
+
+    def show_flashes
+        flash.each do |key, value|
+            content_tag(:div, value, :class => "flash #{key}")
+        end
+    end
 end
